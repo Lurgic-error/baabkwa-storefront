0a3fb465ac5ad8d5c7258880ce20e9befb02023f
Lurgic-error <glngwenya@gmail.com>
2022-02-20 10:42:50 +0300

    added shops

--- src/views/TheShops.vue
+++ src/views/TheShops.vue
@@ -1,9 +1,192 @@
 <template>
     <div class="container the__view">
-        <h1>The Shops</h1>
+        <section class="padding-y bg-white">
+            <div class="container">
+                <!-- ============ COMPONENT BS CARD WITH IMG ============ -->
+                <div class="row gy-3">
+                    <div class="col-lg-4 col-md-6">
+                        <article class="card bg-dark"> <img
+                                src="https://bootstrap-ecommerce.com/https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner1.jpg"
+                                height="160" class="card-img opacity">
+                            <div class="card-img-overlay">
+                                <h5 class="mb-0 text-white">Apple Devices</h5>
+                                <p class="card-text text-white">This is a wider card with a text below</p> <a href="#"
+                                    class="btn btn-light">Discover</a>
+                            </div>
+                        </article> <!-- card .// -->
+                    </div> <!-- col.// -->
+                    <div class="col-lg-4 col-md-6">
+                        <article class="card bg-dark"> <img
+                                src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner2.jpg"
+                                height="160" class="card-img opacity">
+                            <div class="card-img-overlay">
+                                <h5 class="mb-0 text-white">Apple Devices</h5>
+                                <p class="card-text text-white">This is a wider card with a text below</p> <a href="#"
+                                    class="btn btn-light">Discover</a>
+                            </div>
+                        </article> <!-- card .// -->
+                    </div> <!-- col.// -->
+                    <div class="col-lg-4 col-md-6">
+                        <article class="card bg-dark"> <img
+                                src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner3.jpg"
+                                height="160" class="card-img opacity">
+                            <div class="card-img-overlay">
+                                <h5 class="mb-0 text-white">Apple Devices</h5>
+                                <p class="card-text text-white">This is a wider card with a text below</p> <a href="#"
+                                    class="btn btn-light">Discover</a>
+                            </div>
+                        </article> <!-- card .// -->
+                    </div> <!-- col.// -->
+                </div> <!-- row.// -->
+                <!-- ============ COMPONENT BS CARD IMG END .// ============ --><br><br>
+                <div class="row gy-3">
+                    <div class="col-lg-4 col-md-6">
+                        <!-- ============ COMPONENT BANNER 1 ============ -->
+                        <article class="card-banner"
+                            style="height:220px; background-image: url('https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner4.jpg');">
+                            <div class="card-body caption">
+                                <h5 class="card-title text-white">Men</h5>
+                                <p>No matter how far along you are in your sophistication as an amateur astronomer,
+                                    there is always one.</p> <a href="#" class="btn btn-primary"> View more </a>
+                            </div>
+                        </article> <!-- ============ COMPONENT BANNER 1 END .// ============ -->
+                    </div> <!-- col.// -->
+                    <div class="col-lg-4 col-md-6">
+                        <!-- ============ COMPONENT BANNER 2 ============ -->
+                        <article class="card-banner"
+                            style="height:220px; background-image: url('https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner5.jpg');">
+                            <div class="card-body caption">
+                                <h5 class="card-title text-white">Women</h5>
+                                <p>No matter how far along you are in your sophistication as an amateur astronomer,
+                                    there is always one.</p> <a href="#" class="btn btn-primary"> View more </a>
+                            </div>
+                        </article> <!-- ============ COMPONENT BANNER 2 END .// ============ -->
+                    </div> <!-- col.// -->
+                    <div class="col-lg-4 col-md-6">
+                        <!-- ============ COMPONENT BANNER 3 ============ -->
+                        <article class="card-banner"
+                            style="height:220px; background-image: url('https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner6.jpg');">
+                            <div class="card-img-overlay caption">
+                                <h5 class="card-title text-white">Kids</h5>
+                                <p>No matter how far along you are in your sophistication as an amateur astronomer,
+                                    there is always one.</p> <a href="#" class="btn btn-primary"> View more </a>
+                            </div>
+                        </article> <!-- ============ COMPONENT BANNER 3 END .// ============ -->
+                    </div> <!-- col.// -->
+                </div> <!-- row.// --><br><br>
+                <div class="row gy-3">
+                    <div class="col-lg-4 col-md-6">
+                        <!-- ============ COMPONENT BANNER 4 ============ -->
+                        <article class="card-banner"
+                            style="height:250px; background-image: url('https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner7.jpg');">
+                            <div class="text-bottom">
+                                <h5 class="mb-0 text-white"> Men's summer collection <br> Get 80% offer </h5>
+                            </div>
+                        </article><!-- ============ COMPONENT BANNER 4 END .// ============ -->
+                    </div> <!-- col.// -->
+                    <div class="col-lg-4 col-md-6">
+                        <!-- ============ COMPONENT BANNER 5 ============ -->
+                        <article class="card-banner"
+                            style="height:250px; background-image: url('https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner8.jpg');">
+                            <div class="caption p-3 bottom">
+                                <h5 class="card-title text-white">Watches</h5>
+                                <p class="mb-0">No matter how far along you are in your sophistication as an amateur.
+                                </p>
+                            </div>
+                        </article><!-- ============ COMPONENT BANNER 5 END .// ============ -->
+                    </div> <!-- col.// -->
+                    <div class="col-lg-4 col-md-6">
+                        <!-- ============ COMPONENT BANNER 6 ============ -->
+                        <article class="card-banner"
+                            style="height:250px; background-image: url('https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner9.jpg');">
+                            <div class="caption p-3 mt-auto m-4 w-100">
+                                <h5 class="card-title text-white">Camera &amp; Photo</h5>
+                                <p class="mb-0">No matter how far along you are in your sophistication as an amateur.
+                                </p>
+                            </div>
+                        </article><!-- ============ COMPONENT BANNER 6 END .// ============ -->
+                    </div> <!-- col.// -->
+                </div> <!-- row.// --><br><br>
+                <div class="row gy-3">
+                    <div class="col-lg-8">
+                        <!-- ============ COMPONENT BANNER 7 ============ -->
+                        <div class="card-banner overlay-gradient"
+                            style="min-height:230px; background-image: url('https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner1.jpg');">
+                            <div class="card-img-overlay">
+                                <h3 class="card-title text-white">T-Shirts &amp; Jackets</h3>
+                                <p class="card-text text-white" style="max-width: 400px"> No matter how far along you
+                                    are in your sophistication as an amateur astronomer, there is always one.</p> <a
+                                    href="" class="btn btn-warning">Learn more</a>
+                            </div>
+                        </div><!-- ============ COMPONENT BANNER 7 END .// ============ -->
+                    </div> <!-- col.// -->
+                    <div class="col-lg-4 col-md-6">
+                        <!-- ============ COMPONENT BANNER 8 ============ -->
+                        <div class="card-banner overlay-gradient"
+                            style="min-height:230px; background-image: url('https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/posts/3.jpg');">
+                            <div class="card-img-overlay">
+                                <h5 class="card-title text-white">Best products</h5>
+                                <p class="card-text text-white">No matter how far along you are in your sophistication
+                                    as an amateur astronomer, there is always one.</p>
+                            </div>
+                        </div><!-- ============ COMPONENT BANNER 8 END .// ============ -->
+                    </div> <!-- col.// -->
+                </div> <!-- row.// --><br><br>
+                <div class="row gy-3">
+                    <div class="col-lg-4 col-md-6">
+                        <!-- ================ COMPONENT ITEM BG ================ -->
+                        <div class="card card-banner">
+                            <div class="p-4" style="width:70%">
+                                <h5 class="title">iPad Pro</h5>
+                                <p class="mb-0">Some quick example text to build on the card title and make up the bulk
+                                    info</p>
+                            </div> <img
+                                src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner-item1.jpg"
+                                height="150" class="img-bg">
+                        </div> <!-- ================ COMPONENT ITEM BG END .// ================ -->
+                    </div> <!-- col.// -->
+                    <div class="col-lg-4 col-md-6">
+                        <!-- ================ COMPONENT ITEM BG ================ -->
+                        <div class="card card-banner">
+                            <div class="p-4" style="width:70%">
+                                <h5 class="title">Smart Watch</h5>
+                                <p class="mb-0">Some quick example text to build on the card title and make up the bulk
+                                    info</p>
+                            </div> <img
+                                src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner-item2.jpg"
+                                height="150" class="img-bg">
+                        </div> <!-- ================ COMPONENT ITEM BG END .// ================ -->
+                    </div> <!-- col.// -->
+                    <div class="col-lg-4 col-md-6">
+                        <!-- ================ COMPONENT ITEM BG ================ -->
+                        <div class="card card-banner">
+                            <div class="p-4" style="width:70%">
+                                <h5 class="title">Great drones</h5>
+                                <p class="mb-0">Some quick example text to build on the card title and make up the bulk
+                                    info</p>
+                            </div> <img
+                                src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/banners/banner-item3.jpg"
+                                height="150" class="img-bg">
+                        </div> <!-- ================ COMPONENT ITEM BG END .// ================ -->
+                    </div> <!-- col.// -->
+                </div> <!-- row.// --><br><br>
+                <article class="card p-4 bg-primary">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h4 class="mb-0 text-white">Banner sample heading name</h4>
+                            <p class="mb-0 text-white-50">Trendy products and text to build on the card title</p>
+                        </div>
+                        <div class="col-auto"> <a class="btn btn-warning" href="#">Discover</a> </div>
+                    </div>
+                </article>
+            </div> <!-- container .// -->
+        </section>
     </div>
+
+    <TheFooter/>
 </template>
 
-<script setup></script>
+<script setup>
+import TheFooter from '../components/TheFooter.vue';</script>
 
 <style></style>
