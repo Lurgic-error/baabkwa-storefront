52c05b8e18e18ce824c48f93e0969c1d6e0e985f
Lurgic-error <glngwenya@gmail.com>
2022-02-22 20:33:25 +0300

    new build

--- dist/assets/TheLogout.77872b59.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.635a48c9.js";import{o,c}from"./vendor.bc2fcb6b.js";const r={},t={class:"container"};function n(s,a){return o(),c("div",t)}var i=e(r,[["render",n]]);export{i as default};
